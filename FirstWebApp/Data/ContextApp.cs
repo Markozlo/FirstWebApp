d390d6a0ec5da4a8d84fc7a767ee56cff5c450ec
Markozlo <marcelo.fersan@hotmail.com>
2025-03-24 19:12:43 -0300

    Repository Update

---
+++ FirstWebApp/Data/ContextApp.cs
@@ -0,0 +1,30 @@
+﻿using FirstWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace FirstWebApp.Data
+{
+    public class ContextApp : DbContext
+    {
+        public ContextApp() : base("DefaultConnection")
+        {
+        
+        }
+    
+        public virtual DbSet<Product> product { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Properties<string>().Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties().Where(p => p.Name == "Id").Configure(p => p.IsKey());
+
+        }
+    }
+}
