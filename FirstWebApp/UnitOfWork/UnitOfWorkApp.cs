9ed7e777589cda41b42b900fe58745b45f79373a
Markozlo <marcelo.fersan@hotmail.com>
2025-03-27 16:12:44 -0300

    Update

---
+++ FirstWebApp/UnitOfWork/UnitOfWorkApp.cs
@@ -0,0 +1,31 @@
+﻿using FirstWebApp.Data;
+using FirstWebApp.Models;
+using FirstWebApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FirstWebApp.UnitOfWork
+{
+    public class UnitOfWorkApp : DbContext
+    {
+        ContextApp context = new ContextApp();
+        Repository<Product> productRepository;
+
+        public Repository<Product> ProductRepository
+        { 
+            get
+            { 
+                if (productRepository == null)
+                {
+                    productRepository = new Repository<Product>(context);
+
+                }
+                return productRepository; 
+            } 
+        }
+
+    }
+}
